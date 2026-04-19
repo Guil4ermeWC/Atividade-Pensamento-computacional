5944a466af95621cf545e5dfd3179ad0c0bda748
Guil4ermeWC <guiguiender123456@gmail.com>
2026-04-19 20:13:30 -0300

    Create circuitocodico.py

---
+++ Trabalho.02.04/circuitocodico.py
@@ -0,0 +1,18 @@
+## 4. Montagem do Circuito
+
+Aqui está a captura de tela do circuito montado no Tinkercad:
+
+![Circuito do Portão Automático](circuito.png)
+
+---
+
+## 5. Print do Código
+
+Abaixo, a captura da lógica implementada:
+
+![Print do Código Tinkercad](codico1.png)
+![Print do Código Tinkercad](codico2.png)
+![Print do Código Tinkercad](codico3.png)
+
+> **Nota:** O código-fonte completo também pode ser encontrado no arquivo `projeto.ino` deste repositório.
+  
